dda8d2c5b1328f11519e60d6ada95f41f6e3e69d
roggc <roggc9@gmail.com>
2022-12-30 21:35:46 +0100

    two routes

--- src/main.mjs
+++ src/main.mjs
@@ -14,11 +14,23 @@ app.use(cors(corsOptions));
 
 const weatherForecastUrl = "api.openweathermap.org/data/2.5/onecall";
 
-app.get("/data", async (req, res) => {
-  const queryString = Object.entries(req.query)
+const getParamsString = (query) =>
+  Object.entries(query)
     .reduce((result, [key, value]) => result.concat(`${key}=${value}&`), "")
     .concat(`appid=${process.env.API_KEY}`);
+
+app.get("/current", async (req, res) => {
+  const queryString = getParamsString(req.query);
   const resp = await fetch(`https://${weatherForecastUrl}?${queryString}`);
+  const data = await resp.json();
+  res.json(data);
+});
+
+app.get("/timemachine", async (req, res) => {
+  const queryString = getParamsString(req.query);
+  const resp = await fetch(
+    `https://${weatherForecastUrl}/timemachine?${queryString}`
+  );
   const data = await resp.json();
   res.json(data);
 });
